6dff44bfdbd16d82b8f789a8ea46855273240dd2
osi1880vr <87379616+osi1880vr@users.noreply.github.com>
2022-09-15 11:28:06 +0200

    Create setup.py
    .

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AdaBins',
+    version='0.0.1',
+    description='Official implementation of Adabins: Depth Estimation using adaptive bins',
+    packages=find_packages(),
+    install_requires=[],
+)
